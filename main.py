d5a00735d026f658a86f39de1dc4fd7c6dee3db2
7enTropy7 <menon.uk1998@gmail.com>
2020-06-22 01:56:56 +0530

    Final commit

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ bit_length = int(input("Enter Key Bit Length: "))
 
 public, private = RSA_module.generate_keypair(bit_length)
 
-msg = input("Write msg: ")
+message = input("Enter Message: ")
 
 t1 = time.time()
-encrypted_msg = RSA_module.encrypt(msg, public)
-print("Encrypted msg: " + str(encrypted_msg))
+encrypted_message = RSA_module.encrypt(message, public)
+print("Encrypted Message: " + str(encrypted_message))
 
-decrypted_msg = RSA_module.decrypt(encrypted_msg, private)
-print("Decrypted msg: " + str(decrypted_msg))
+decrypted_message = RSA_module.decrypt(encrypted_message, private)
+print("Decrypted Message: " + str(decrypted_message))
 print('Runtime : ' + str(time.time()-t1) + ' seconds')
